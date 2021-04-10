a6d5ef6da6000aabc3391e79f01795a7f4425c25
Signe <sign4731@stud.kea.dk>
2021-04-10 12:41:00 +0200

    mouse follower ready for testing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   runTextAnimation();
+  startMouseFollower();
 }
 
 function runTextAnimation() {
@@ -31,5 +32,30 @@ function runTextAnimation() {
     opacity: 1,
     y: 0,
     ease: "back.out(1)",
+  });
+}
+
+function startMouseFollower() {
+  gsap.set(".mouse_follower", { xPercent: -50, yPercent: -50 });
+
+  const ball = document.querySelector(".mouse_follower");
+  const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+  const mouse = { x: pos.x, y: pos.y };
+  const speed = 0.35;
+
+  const xSet = gsap.quickSetter(ball, "x", "px");
+  const ySet = gsap.quickSetter(ball, "y", "px");
+
+  window.addEventListener("mousemove", (e) => {
+    mouse.x = e.x;
+    mouse.y = e.y;
+  });
+  gsap.ticker.add(() => {
+    const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+    pos.x += (mouse.x - pos.x) * dt;
+    pos.y += (mouse.y - pos.y) * dt;
+    xSet(pos.x);
+    ySet(pos.y);
   });
 }
